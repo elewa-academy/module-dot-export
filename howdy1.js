3938a5fc9bc6bb58473a7d1dc10f34c8b5366956
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2018-08-20 11:02:32 +0200

    Update howdy1.js

--- howdy1.js
+++ howdy1.js
@@ -1,4 +1,4 @@
-// when trying howdy1 in your server, try consolelogging 'howdy' to see what it is
+// when running howdy1 in your terminal, try consolelogging 'howdy' to see what it is
 // calling 'howdy' runs this file top to bottom like any old script
 
 
