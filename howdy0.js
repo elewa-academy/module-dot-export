a5b47fad361b2f2eeb551767eff44b9257b8659e
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2018-08-20 11:02:03 +0200

    no more server

--- howdy0.js
+++ howdy0.js
@@ -1,4 +1,4 @@
-// when trying howdy0 in your server, try consolelogging 'howdy' to see what it is
+// when running howdy0 in your terminal, try consolelogging 'howdy' to see what it is
 
 console.log('howdy');
 console.log('howdy');
